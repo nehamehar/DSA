293f68b19df33063c562bf4f2c74479819d85df9
Neha Mehar <nehamehar31@gmail.com>
2025-04-10 01:37:08 +0530

    Done qes no. 2665

---
+++ 30 Days of JavaScript/4) 2665. Counter II.js
@@ -0,0 +1,16 @@
+function createCounter(init){
+    let inIT = init
+    return {
+        increment: function () {
+      return inIT+=1;
+    },
+    reset: function () {
+      inIT = init;
+      return inIT;
+    },
+    decrement: function () {
+      return --inIT;
+    }
+}
+}
+console.log (createCounter());
