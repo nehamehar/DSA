801a296683f583ba7110bc1db305384370d6800b
Neha Mehar <nehamehar31@gmail.com>
2025-04-11 03:33:59 +0530

    Done qes no. 2635

--- 30 Days of JavaScript/5) 2635. Apply Transform Over Each Element in Array.js
+++ 30 Days of JavaScript/5) 2635. Apply Transform Over Each Element in Array.js
@@ -1,7 +1,7 @@
-let map = function(arr, fn) {
+let map = function(arr, fn) {   // fn is rule we are passing in fun for array
     const transformedArr = [];
     for (let i = 0; i < arr.length; i++) {
       transformedArr[i] = fn(arr[i], i);
     }
     return transformedArr;
-  };
+  };  
