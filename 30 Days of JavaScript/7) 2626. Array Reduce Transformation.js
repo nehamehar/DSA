0caf27929c0fbd07ab52f231f38c8112d9446904
Neha Mehar <nehamehar31@gmail.com>
2025-04-22 23:41:10 +0530

    Done 2626

---
+++ 30 Days of JavaScript/7) 2626. Array Reduce Transformation.js
@@ -0,0 +1,7 @@
+var reduce = function(nums, fn, init) {
+    let res = init // starts arr from initial value and updating it
+    for (let i=0; i<nums.length; i++){
+        res = fn(res, nums[i])
+    }
+    return res;
+};
