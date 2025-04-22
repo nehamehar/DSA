daedbd075cd9bbaa7d08dea8afebed24a48090cc
Neha Mehar <nehamehar31@gmail.com>
2025-04-22 23:39:46 +0530

    Done 2634

---
+++ 30 Days of JavaScript/6) 2634. Filter Elements from Array.js
@@ -0,0 +1,8 @@
+var filter = function(arr, fn) {
+    const newarr = [];
+    for (let i=0; i<arr.length; i++) {
+        if (fn(arr[i], i))
+            newarr.push(arr[i])
+    }
+    return newarr
+};
